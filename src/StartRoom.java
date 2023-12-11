bc8f63c46c9919976ef678027850a7d86f2ecddb
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2023-12-11 12:25:39 +0100

    Create StartRoom.java
    Added the StartRoom class and following methods

---
+++ src/StartRoom.java
@@ -0,0 +1,24 @@
+public class StartRoom extends ARoom {
+    private boolean candleLit = false;
+    private boolean bookshelfPuzzleSolved = false;
+    private boolean paintingPuzzleSolved = false;
+
+    public void enter() {
+
+    }
+
+    public void runRoomLoop() {
+
+    }
+
+    private void startBookshelfPuzzle() {
+
+    }
+
+    private void startPaintingPuzzle() {
+
+    }
+    private void exit() {
+
+    }
+}
