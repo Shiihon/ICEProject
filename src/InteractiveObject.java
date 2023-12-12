0ef3f7c6a6a6d055c2ffc0006b0f3e0854c4ef2d
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2023-12-12 17:24:53 +0100

    Use InteractiveType and add getters

--- src/InteractiveObject.java
+++ src/InteractiveObject.java
@@ -1,10 +1,23 @@
 public class InteractiveObject {
 
-    private final String type;
+    private final InteractiveType type;
     private final String name;
 
-    public InteractiveObject(String type, String name) {
+    public InteractiveObject(InteractiveType type, String name) {
         this.type = type;
         this.name = name;
+    }
+
+    public InteractiveType getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
