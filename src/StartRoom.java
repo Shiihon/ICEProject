05477f667794e0f8c5aa30c63b69a9e9feab2332
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2023-12-12 17:53:47 +0100

    Merge

--- src/StartRoom.java
+++ src/StartRoom.java
@@ -19,10 +19,8 @@ public class StartRoom extends ARoom {
 
     @Override
     public void enter() {
-        System.out.println();
-        System.out.println("You've entered the first room in the mansion.");
-
-        System.out.println("You have entered the mansion. You see numerous objects in the entrance and a locked door.");
+        TextUI.displayMessage();
+        TextUI.displayMessage("You have entered the mansion. You see numerous objects in the entrance and a locked door.");
 
         runRoomLoop();
     }
@@ -90,54 +88,44 @@ public class StartRoom extends ARoom {
     }
 
     private void approachBookshelf() {
+        TextUI.displayMessage();
         TextUI.displayMessage("You begin walking to the big bookshelf");
 
-        if(!candleLit)
-        {
-            TextUI.displayMessage("you see a lot of books on the shelf but you can't read and not even make out any of the titles.");
-
+        if (!candleLit) {
+            TextUI.displayMessage("You see a lot of books on the shelf but you can't read and not even make out any of the titles.");
+            return;
         }
 
-        if(candleLit)
-        {
-            TextUI.displayMessage(" You see a lot of books: “IT, Saw, American Psycho, Sinister”. At the top of the shelf there is a sign “Horror”. You quickly realize that all the books are old and new Disney movies and you ask yourself “Why”?\n" +
-                    "Some of the books are very old and others new. There are some that catch your eye more than others. The Exorcist, Smile, The nun and Scream . \n");
+        TextUI.displayMessage("You see a lot of books: “IT, Saw, American Psycho, Sinister”. At the top of the shelf there is a sign “Horror”. You quickly realize that all the books are old and new Disney movies and you ask yourself “Why”?\n" +
+                "Some of the books are very old and others new. There are some that catch your eye more than others. The Exorcist, Smile, The nun and Scream.");
 
-            List<String> options = new ArrayList<>();
+        List<String> options = new ArrayList<>();
 
-            options.add("The Exorcsit");
-            options.add("Scream");
-            options.add("The nun");
-            options.add("Smile");
-            options.add("Look around");
+        options.add("The Exorcist");
+        options.add("Scream");
+        options.add("The nun");
+        options.add("Smile");
+        options.add("Look around");
 
-            int choice = TextUI.getChoice("What do you want to do?",options);
+        int choice = TextUI.getChoice("What do you want to do?", options);
 
-            if(choice == options.indexOf("Smile"))
-            {
-                TextUI.displayMessage(" You take "+ choice +" out the bookshelf. After 3 sek, you hear a loud noise, the bookshelf starts moving. Another 10 sec goes by, the bookshelf has now moved 1meter to the left revealing a keypad");
-                List<String>Key = new ArrayList<>();
-                Key.add("NumberPad");
-                Key.add("Look around");
-                int input = TextUI.getChoice("What do you want to do", Key);
+        if (choice == options.indexOf("Smile")) {
+            TextUI.displayMessage("You take Smile out the bookshelf. After 3 seconds, you hear a loud noise, the bookshelf starts moving. Another 10 sec goes by, the bookshelf has now moved 1meter to the left revealing a keypad");
+            List<String> Key = new ArrayList<>();
+            Key.add("NumberPad");
+            Key.add("Look around");
 
-                if(input == Key.indexOf("NumberPad"))
-                {
-                    TextUI.displayMessage("You take a step closer to the number pad, there is number form 1-9 and a green button on it, with the word “confirm” on it. You see there are some options, but  don’t really know what to do or what’s gonna happen. Or what will be if you press the wrong numbers, so you decide not to do anything right now and look around again.");
-                    runRoomLoop();
-                } else
-                {
-                    TextUI.displayMessage("You take a Step back start looking around agian");
-                    runRoomLoop();
-                }
+            int input = TextUI.getChoice("What do you want to do", Key);
 
-            } else if (choice == options.indexOf("Look around"))
-            {
-                runRoomLoop();
+            if (input == Key.indexOf("NumberPad")) {
+                TextUI.displayMessage("You take a step closer to the number pad, there is number form 1-9 and a green button on it, with the word “confirm” on it. You see there are some options, but don’t really know what to do or what’s gonna happen. Or what will be if you press the wrong numbers, so you decide not to do anything right now and look around again.");
+                // Type on the keypad
             } else {
-                TextUI.displayMessage("you take " + choice + " the book is full of dust. you start going through the pages reading the story, but nothing happens. You put the book back on the shelf, take a step back, you're looking at the bookshelf again.");
-                approachBookshelf();
+                TextUI.displayMessage("You take a Step back start looking around again");
             }
+        } else if (choice != options.indexOf("Look around")) {
+            TextUI.displayMessage("You take " + options.get(choice) + " the book is full of dust. You start going through the pages reading the story, but nothing happens. You put the book back on the shelf, take a step back, you're looking at the bookshelf again.");
+            approachBookshelf();
         }
     }
 
@@ -147,6 +135,6 @@ public class StartRoom extends ARoom {
 
     @Override
     public void exit() {
-        System.out.println("Exit room");
+        TextUI.displayMessage("Exit room");
     }
 }
