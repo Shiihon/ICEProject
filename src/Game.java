8f1ba652e93d04a9d53a0717633123bfe9a2f309
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2023-12-14 11:00:18 +0100

    Add story

--- src/Game.java
+++ src/Game.java
@@ -44,15 +44,42 @@ public class Game {
     }
 
     public void newGame() {
-        String playerName = TextUI.getInput("What's your name?");
+        TextUI.displayMessage();
+        TextUI.displayMessage(
+                """
+                        Welcome, brave soul, to The Haunted Mansion – an escape room like no other.
+                        An escape room that will test you, with different puzzles and obstacles.
+                        A lot of things to investigate, discover and explore.
+                                                
+                        But be quick. Time is of the essence. Someone or something may be watching you, waiting...
+                        """);
 
+        TextUI.getInput("Press Enter to continue...");
+
+        TextUI.displayMessage();
+        TextUI.displayMessage("""
+                Welcome to The Haunted Mansion - Escape Room.
+                                
+                I’m the Gamemaster and I will try my best to help you out where I can.
+                But I don’t have all the answers. Some things you have to figure out on your own.
+                In the mansion, you will find a lot of different items, clues and objects, remember them. They're important.
+                Also, don't be stupid. Don't trust anyone and don’t investigate strange sounds coming from the dark.
+                Or do it, see what happens hehe...
+                The worst that can happen is you getting killed, no biggie.
+                
+                But before we go any further.
+                """);
+
+        String playerName = TextUI.getInput("What's your name?");
         player = new Player(playerName);
 
         TextUI.displayMessage();
-        TextUI.displayMessage("Welcome " + player.getPlayerName() + ", to The Haunted Mansion.");
+        TextUI.displayMessage("Well " + player.getPlayerName() + ", there's no turning back now.");
+        TextUI.displayMessage("""
+                Good luck and try not to die %s
+                """.formatted(playerName));
 
-        // Write story here
-        TextUI.displayMessage("You find yourself deep in the woods and see an old mansion. you approach the mansion.");
+        TextUI.getInput("Press Enter to continue...");
     }
 
     public void quit() {
