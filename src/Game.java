a887dbab4a1a6e53c720c3a3a6d10828aef1297a
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2023-12-14 12:18:43 +0100

    Refactoring

--- src/Game.java
+++ src/Game.java
@@ -45,39 +45,34 @@ public class Game {
 
     public void newGame() {
         TextUI.displayMessage();
-        TextUI.displayMessage(
-                """
-                        Welcome, brave soul, to The Haunted Mansion – an escape room like no other.
-                        An escape room that will test you, with different puzzles and obstacles.
-                        A lot of things to investigate, discover and explore.
-                                                
-                        But be quick. Time is of the essence. Someone or something may be watching you, waiting...
-                        """);
+        TextUI.displayMessage();
+        TextUI.displayMessage("""
+                Welcome, brave soul, to "The Haunted Mansion" – an escape room like no other.
+                I’m the GameMaster and I will try to help you where I can. But I don’t have all the answers.
+                There are some things you have to figure out on your own.
+                                        
+                You will be tested in different puzzles and obstacles.
+                Objects and items will come up along the way. Remember them. They’re important.
+                                        
+                But don’t take too long.
+                Time is of the essence, someone or something may be watching you, waiting...
+                """);
 
         TextUI.getInput("Press Enter to continue...");
 
         TextUI.displayMessage();
         TextUI.displayMessage("""
-                Welcome to The Haunted Mansion - Escape Room.
-                                
-                I’m the Gamemaster and I will try my best to help you out where I can.
-                But I don’t have all the answers. Some things you have to figure out on your own.
-                In the mansion, you will find a lot of different items, clues and objects, remember them. They're important.
-                Also, don't be stupid. Don't trust anyone and don’t investigate strange sounds coming from the dark.
-                Or do it, see what happens hehe...
-                The worst that can happen is you getting killed, no biggie.
-                
-                But before we go any further.
-                """);
+                But before we begin.""");
 
         String playerName = TextUI.getInput("What's your name?");
         player = new Player(playerName);
 
         TextUI.displayMessage();
-        TextUI.displayMessage("Well " + player.getPlayerName() + ", there's no turning back now.");
         TextUI.displayMessage("""
-                Good luck and try not to die %s
-                """.formatted(playerName));
+                Well %s, it's very nice to meet you.
+                Unfortunately we're out of time. There's no turning back now.
+                Good luck and try not to die.
+                """.formatted(player.getName()));
 
         TextUI.getInput("Press Enter to continue...");
     }
