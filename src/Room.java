eb2a7969a57b75ef9213d4acc4c9fece5336f67b
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2023-12-15 11:53:08 +0100

    Add JavaDoc

--- src/Room.java
+++ src/Room.java
@@ -1,10 +1,26 @@
 public interface Room {
 
+    /**
+     * Enter the room
+     */
     void enter();
 
+    /**
+     * Exit the room
+     */
     void exit();
 
+    /**
+     * Get the state of completion of the room
+     *
+     * @return Whether the room is complete or not
+     */
     boolean isComplete();
 
+    /**
+     * Get the total time spend in the room in milliseconds
+     *
+     * @return The time spend in the room in milliseconds
+     */
     long getTimeSpend();
 }
