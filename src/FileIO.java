7c86bbc45260bdc68652344d3f3c9613e8171d8e
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2023-12-16 01:48:07 +0100

    Add file safety checks

--- src/FileIO.java
+++ src/FileIO.java
@@ -7,34 +7,57 @@ import java.util.List;
 import java.util.Scanner;
 
 public final class FileIO {
+
     public static List<String> readScoreData(String path) {
         ArrayList<String> data = new ArrayList<>();
         File file = new File(path);
+
         try {
             Scanner scan = new Scanner(file);
             scan.nextLine(); //Skip header
+
             while (scan.hasNextLine()) {
                 String s = scan.nextLine();
                 data.add(s);
             }
         } catch (FileNotFoundException e) {
-            System.out.println("file not found");
+            TextUI.displayErrorMessage("Error: File " + file.getParentFile().getName() + "/" + file.getName() + " not found.");
         }
         return data;
     }
 
-    public static void saveScoreData(List<Player> players) {
+    public static void saveScoreData(String path, List<Player> players) {
+        File file = new File(path);
+
+        if (ensureFileExistence(file)) {
+            TextUI.displayErrorMessage("Error: Could not write write data to file " + file.getParentFile().getName() + "/" + file.getName() + ".");
+        }
 
         try {
-            FileWriter writer = new FileWriter("data/scoreData");
-            writer.write("Name,Score" + "\n");
+            FileWriter writer = new FileWriter(file);
+            writer.write("Name,Score\n");
+
             for (Player p : players) {
                 String textTosave = p.getName() + "," + p.getTimeScore();
                 writer.write(textTosave + "\n");
             }
             writer.close();
         } catch (IOException e) {
-            System.out.println("noget gik galt ved skrivning til fil");
+            TextUI.displayErrorMessage("Error: Something went wrong while writing user data to file " + file.getParentFile().getName() + "/" + file.getName() + ".");
+        }
+    }
+
+    private static boolean ensureFileExistence(File file) {
+        if (!file.getParentFile().exists() && !file.getParentFile().mkdirs()) {
+            return false;
+        }
+
+        try {
+            file.createNewFile();
+            return true;
+        } catch (IOException e) {
+            TextUI.displayErrorMessage("Error: Could not create file " + file.getParentFile().getName() + "/" + file.getName());
+            return false;
         }
     }
 }
